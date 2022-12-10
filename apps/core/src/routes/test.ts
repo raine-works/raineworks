e4615c6858e601f92119779e146c443f6215421d
raineworks <raine@raine.works>
2022-12-10 02:38:48 -0700

    setting up docker

--- apps/core/src/routes/test.ts
+++
@@ -1,7 +0,0 @@
-import { Router, Request, Response } from 'express'
-
-export const router = Router()
-
-router.get('/', (req: Request, res: Response) => {
-	res.status(200).json('Hello world')
-})
--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,24 +1,26 @@
-import { component$, $ } from '@builder.io/qwik'
+import { component$, $, useStore } from '@builder.io/qwik'
 import type { DocumentHead } from '@builder.io/qwik-city'
 
-export let data: null | string = null
-export const getData = $( async () => {
-	const response = await fetch('http://localhost:8000/test', {
-		method: 'GET',
-		headers: {
-			'Content-Type': 'application/json'
+export default component$(() => {
+	const state = useStore({
+		data: null
+	})
+
+	const getData = $(async () => {
+		const response = await fetch(`${import.meta.env.VITE_API_URL}/test`, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+		if (response.status === 200) {
+			state.data = await response.json()
 		}
 	})
-	if (response.status === 200) {
-		data = await response.json()
-	}
-})
-	
 
-export default component$(() => {
 	return (
 		<div>
-			<h1>{data}</h1>
+			<h1>{state.data}</h1>
 			<button onClick$={getData}>Click me</button>
 		</div>
 	)
