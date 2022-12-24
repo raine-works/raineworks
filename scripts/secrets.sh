b3fac1ae7bcd51f6a5253740915bb420e1e7b954
raineworks <raine@raine.works>
2022-12-24 02:17:20 -0700

    tlc

--- scripts/secrets.sh
+++ scripts/secrets.sh
@@ -2,4 +2,4 @@ echo "Getting secret variables"
 
 find -type f -name '*.env*' -delete
 
-echo "VITE_API_URL=http://raineworks.com/api" >> apps/web/.env
+echo "VITE_API_URL=https://raineworks.com/api" >> apps/web/.env
